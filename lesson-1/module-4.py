a04260d5d837df0ae357bfd6cf4bf06cac099ff1
Freesty7er <ian.anfalov@gmail.com>
2020-10-01 17:54:10 +0300

    1

--- lesson-1/module-4.py
+++ lesson-1/module-4.py
@@ -10,15 +10,3 @@ while int_number > 0:
     int_number = int_number // 10
 
 print("max number %d"%max_number)
-int_number = int(input("enter int number"))
-max_number = 0
-
-while int_number > 0:
-    k = int_number % 10
-
-    if k > max_number:
-        max_number = k
-
-    int_number = int_number // 10
-
-print("max number %d"%max_number)
